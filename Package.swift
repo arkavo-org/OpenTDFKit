833c808db171669d59aeb6959dee7ff2a2a6f04f
Paul Flynn <paul@arkavo.com>
2024-06-08 20:59:03 -0400

    Update supported platforms and Xcode setup in CI pipeline
    The platforms section in Package.swift has been updated to reflect the latest versions of macOS, iOS, tvOS, and watchOS. In addition, the CI workflow now uses the latest stable version of Xcode for building and testing.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 let package = Package(
     name: "NanoTDF",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v13), // Update to the latest macOS version
+        .iOS(.v16), // Update to the latest iOS version
+        .tvOS(.v16), // Update to the latest tvOS version
+        .watchOS(.v9) // Update to the latest watchOS version
     ],
     products: [
         .library(
